b04e4c1adae74327d07f47aeb35d11530da7f859
SoggyCereal <PhillipsJo860@northwested.org>
2025-02-25 10:35:27 -0500

    Update game.py

--- game.py
+++ game.py
@@ -19,22 +19,28 @@ def handle_events():
 
 def draw_rect(screen, rect, color, thickness):
             pygame.draw.rect(screen, color, rect, thickness)
-# Write this function above your main( ) function
+
 def draw_circle(screen, center, radius, color, thickness):
-    """Draws a circle on the Pygame window."""
     pygame.draw.circle(screen, color, center, radius, thickness)
 
-
-        
-
-# Call the function that draws the circle from your main() function
-
-
-# Write function above your main( ) function
 def draw_line(surface, color, start_pos, end_pos, thickness):
     pygame.draw.line(surface, color, start_pos, end_pos, thickness)
 
+def draw_poly(screen, color, points, thickness=0):
+     pygame.draw.polygon(screen, color, points, thickness)
     
+topgrid_pos = {'start1': (1, 0), 'start2': (100, 0), 'start3': (200, 0), 'start4': (300, 0), 'start5': (400, 0), 'start6': (500, 0), 'start7': (600, 0), 'start8': (700, 0), 'start9': (799, 0), 'end1': (1, 20), 'end2': (100, 20), 'end3': (200, 20), 'end4': (300, 20), 'end5': (400, 20), 'end6': (500, 20), 'end7': (600, 20), 'end8': (700, 20), 'end9': (799, 20)}
+sidegrid_pos = {'start1': (0, 1), 'start2': (0, 100), 'start3': (0, 200), 'start4': (0, 300), 'start5': (0, 400), 'start6': (0, 500), 'start7': (0, 600), 'end1': (20, 1), 'end2': (20, 100), 'end3': (20, 200), 'end4': (20, 300), 'end5': (20, 400), 'end6': (20, 500), 'end7': (20, 600)}
+
+grid_thick = 3
+
+polly_points1 = [
+     (500, 100),
+     (600, 350),
+     (550, 400),
+     (650, 400),
+     (700, 250)
+]
 
 
 def main():
@@ -44,6 +50,25 @@ def main():
     while running:
         running = handle_events()
         screen.fill(config.COLOR_WHITE)
+
+        # Top grid
+        draw_line(screen, config.COLOR_BLACK, topgrid_pos['start1'], topgrid_pos['end1'], grid_thick)
+        draw_line(screen, config.COLOR_BLACK, topgrid_pos['start2'], topgrid_pos['end2'], grid_thick)
+        draw_line(screen, config.COLOR_BLACK, topgrid_pos['start3'], topgrid_pos['end3'], grid_thick)
+        draw_line(screen, config.COLOR_BLACK, topgrid_pos['start4'], topgrid_pos['end4'], grid_thick)
+        draw_line(screen, config.COLOR_BLACK, topgrid_pos['start5'], topgrid_pos['end5'], grid_thick)
+        draw_line(screen, config.COLOR_BLACK, topgrid_pos['start6'], topgrid_pos['end6'], grid_thick)
+        draw_line(screen, config.COLOR_BLACK, topgrid_pos['start7'], topgrid_pos['end7'], grid_thick)
+        draw_line(screen, config.COLOR_BLACK, topgrid_pos['start8'], topgrid_pos['end8'], grid_thick)
+        draw_line(screen, config.COLOR_BLACK, topgrid_pos['start9'], topgrid_pos['end9'], grid_thick)
+        # Side grid
+        draw_line(screen, config.COLOR_BLACK, sidegrid_pos['start1'], sidegrid_pos['end1'], grid_thick)
+        draw_line(screen, config.COLOR_BLACK, sidegrid_pos['start2'], sidegrid_pos['end2'], grid_thick)
+        draw_line(screen, config.COLOR_BLACK, sidegrid_pos['start3'], sidegrid_pos['end3'], grid_thick)
+        draw_line(screen, config.COLOR_BLACK, sidegrid_pos['start4'], sidegrid_pos['end4'], grid_thick)
+        draw_line(screen, config.COLOR_BLACK, sidegrid_pos['start5'], sidegrid_pos['end5'], grid_thick)
+        draw_line(screen, config.COLOR_BLACK, sidegrid_pos['start6'], sidegrid_pos['end6'], grid_thick)
+        draw_line(screen, config.COLOR_BLACK, sidegrid_pos['start7'], sidegrid_pos['end7'], grid_thick)
 
         # Rectangle
         myrect1 = [100, 100, 200, 150]
@@ -60,6 +85,9 @@ def main():
         # Line
         draw_line(screen, config.COLOR_DARKBLUE, [100, 100], [700, 500], 5)  
         draw_line(screen, config.COLOR_CRIMSON, [75, 250], [500, 300], 2)
+
+        # Polygon
+        draw_poly(screen, config.COLOR_DARKSLATEBLUE, polly_points1)
         
 
         pygame.display.flip()
